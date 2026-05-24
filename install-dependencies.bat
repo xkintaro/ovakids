bc355ba8bd80eb9ba8b9d4a57668eb83c922728e
xkintaro <mustafatasal4242@gmail.com>
2026-05-24 03:53:26 +0300

    added .bat files

---
+++ install-dependencies.bat
@@ -0,0 +1,10 @@
+@echo off
+title Dependency Installer
+
+echo installing...
+call npm install
+
+echo ============================
+echo All dependencies installed!
+echo ============================
+pause
---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo off
+title Frontend Starter
+
+echo Frontend...
+start cmd /k "npm run dev"
+
+echo ============================
+echo all services started!
+echo ============================
+pause
